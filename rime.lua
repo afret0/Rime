be62dd07129dee53fe61fd0227a09c8556fb246b
afreto <kong5664546498@gmail.com>
2026-04-05 13:44:33 +0800

    f

---
+++ rime.lua
@@ -0,0 +1,12 @@
+-- 日期时间输入
+-- 输入 rq 输出当前日期，输入 sj 输出当前时间
+
+function date_time_translator(input, seg)
+  if input == "rq" then
+    local date = os.date("%Y年%m月%d日")
+    yield(Candidate("date", seg.start, seg._end, date, "日期"))
+  elseif input == "sj" then
+    local time = os.date("%H:%M:%S")
+    yield(Candidate("time", seg.start, seg._end, time, "时间"))
+  end
+end
