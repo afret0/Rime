9cbe207de8fb399e73899c89e361ce715939c4a7
afreto <kong5664546498@gmail.com>
2026-04-05 13:58:17 +0800

    f

--- rime.lua
+++ rime.lua
@@ -3,10 +3,14 @@
 
 function date_time_translator(input, seg)
   if input == "rq" then
-    local date = os.date("%Y年%m月%d日")
-    yield(Candidate("date", seg.start, seg._end, date, "日期"))
+    local date = os.date("%Y-%m-%d")
+    local cand = Candidate("date", seg.start, seg._end, date, "日期")
+    cand.quality = 100
+    yield(cand)
   elseif input == "sj" then
-    local time = os.date("%H:%M:%S")
-    yield(Candidate("time", seg.start, seg._end, time, "时间"))
+    local time = os.date("%Y-%m-%d %H:%M:%S")
+    local cand = Candidate("time", seg.start, seg._end, time, "时间")
+    cand.quality = 100
+    yield(cand)
   end
 end
